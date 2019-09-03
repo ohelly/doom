0eb0d42dafadd6a11cad2422b99a09462c2f4c43
Vladislav <rpashtet0815@gmail.com>
2019-09-03 18:52:25 +0300

    upadte

--- editor/src/put_canvas.c
+++ editor/src/put_canvas.c
@@ -6,7 +6,7 @@
 /*   By: ohelly <ohelly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/23 22:07:45 by ohelly            #+#    #+#             */
-/*   Updated: 2019/09/03 18:43:55 by ohelly           ###   ########.fr       */
+/*   Updated: 2019/09/03 18:49:47 by ohelly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,21 @@ void		draw_canvas(t_doom *doom)
 	while (y > 0)
 	{
 		*doom->line = (t_line){ x, y, 0, y, 0, 0 };
-		line(doom);
+		line(doom, 0x999999);
 		y -= doom->sh;
 	}
 	y = HEIGHT / 2;
 	while (y < HEIGHT)
 	{
 		*doom->line = (t_line){ x, y, 0, y, 0, 0 };
-		line(doom);
+		line(doom, 0x999999);
 		y += doom->sh;
 	}
 	y = HEIGHT / 2;
 	while (y > 0)
 	{
 		*doom->line = (t_line){ x, y, WIDTH, y, 0, 0 };
-		line(doom);
+		line(doom, 0x999999);
 		y -= doom->sh;
 	}
 	y = HEIGHT / 2;
@@ -57,20 +57,20 @@ void		draw_canvas(t_doom *doom)
 	while (x > 0)
 	{
 		*doom->line = (t_line){ x, y, x, 0, 0, 0 };
-		line(doom);
+		line(doom, 0x999999);
 		x -= doom->sh;
 	}
 	x = WIDTH / 2;
 	while (x < WIDTH)
 	{
 		*doom->line = (t_line){ x, y, x, 0, 0, 0 };
-		line(doom);
+		line(doom, 0x999999);
 		x += doom->sh;
 	}
 	while (x > 0)
 	{
 		*doom->line = (t_line){ x, y, x, HEIGHT, 0, 0 };
-		line(doom);
+		line(doom, 0x999999);
 		x -= doom->sh;
 	}
 }
