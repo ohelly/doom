27849372af63d383603499512443755169c7f14f
glormell <glormell@e1r11p4.21-school.ru>
2019-10-15 19:00:20 +0300

    draw weapon

--- src/drawweapon.c
+++ src/drawweapon.c
@@ -6,7 +6,7 @@
 /*   By: glormell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 18:08:49 by glormell          #+#    #+#             */
-/*   Updated: 2019/10/15 18:41:03 by glormell         ###   ########.fr       */
+/*   Updated: 2019/10/15 19:00:13 by glormell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,25 @@ void		drawweapon(t_doom *doom, t_weapon *weapon)
 {
 	int		y;
 	int		x;
-	t_xy 	scale;
+	t_xy	scale;
 	t_xy	t;
 	t_img	img;
 
 	img = weapon_get_image(doom, weapon);
-	x = 0;
-	while (x < WIDTH)
+	x = -1;
+	while (++x < WIDTH)
 	{
 		scale.x = (float)img.w / (WIDTH);
 		scale.y = (float)img.h / (HEIGHT);
-		t.y
-		t.x
-		//t.y = player.yaw * 25;
-		//t.x = (x * scale.x) + player.angle * 50;
-		/*if (t.x >= img.w)
+		t.y = 0;
+		t.x = 0;
+		y = -1;
+		while (++y < HEIGHT)
 		{
-			while (t.x >= img.w)
-				t.x -= img.w;
-		}
-		else if (t.x < 0)
-		{
-			while (t.x < 0)
-				t.x += img.w;
-		}*/
-		y = 0;
-		while (y < HEIGHT)
-		{
-			/*if (t.y > img.h)
-				t.y = img.h - t.y;
-			else if (t.y < 0)
-				t.y = img.h + t.y;*/
 			if (doom->visible[y][x] == 1)
-				doom->sdl->pix[y * WIDTH + x] = img.data[(int)t.y * img.w + (int)t.x];
+				doom->sdl->pix[y * WIDTH + x] =
+					img.data[(int)t.y * img.w + (int)t.x];
 			t.y += scale.y;
-			y++;
 		}
-		x++;
 	}
-	return (0);
 }
